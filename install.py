a0e8c3124e4d4c06eaff8c84564e0968e471d965
dman <brazilsworld@icloud.com>
2023-09-13 22:41:56 -0400

    started lab1, updated requirements

--- install.py
+++ install.py
@@ -1,7 +1,14 @@
 import os
+import sys
 
 def install_requirements():
-    os.system('pip install -r requirements.txt')
+    # Check the major version of Python in use
+    if sys.version_info[0] == 3:
+        pip_command = 'pip3'
+    else:
+        pip_command = 'pip'
+    
+    os.system(f'{pip_command} install -r requirements.txt')
 
 if __name__ == "__main__":
     install_requirements()
